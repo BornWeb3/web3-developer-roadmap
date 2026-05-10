bc8ffaef86d6fbddc5b2fa00a8b6d44621437308
BornWeb3 <sashavibornij@gmail.com>
2026-05-10 16:48:46 +0300

    Add VulnerableVault contract with deposit and withdraw functions

---
+++ 46-reentrancy/VulnerableVault.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract VulnerableVault {
+
+    mapping(address => uint256) public balances;
+
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
+
+    function deposit() external payable {
+        require(msg.value > 0, "ZERO_VALUE");
+
+        balances[msg.sender] += msg.value;
+
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw() external {
+        uint256 amount = balances[msg.sender];
+
+        require(amount > 0, "NO_BALANCE");
+
+        (bool success,) = payable(msg.sender).call{value: amount}("");
+        require(success, "ETH_SEND_FAILED");
+
+        balances[msg.sender] = 0;
+
+        emit Withdraw(msg.sender, amount);
+    }
+
+    function vaultBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+}
